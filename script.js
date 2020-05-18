070ea91b8c0091a4b8f0353df4db4ad624dfca47
ithomas91 <ithomas.nemeth@gmail.com>
2020-05-18 15:16:51 +0200

    Create Face Recognition and battery status

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//// OnClick functions /////
 function openFaceRecognition(){
     document.location.href = 'faceRecognition.html';
 }
@@ -5,3 +6,27 @@ function openFaceRecognition(){
 function backToHome(){
     document.location.href = 'index.html';
 }
+
+//// Video /////////////////////
+
+const video = document.getElementById('video')
+
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  ]).then(startVideo)
+  
+
+function startVideo(){
+    navigator.mediaDevices.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+
+
